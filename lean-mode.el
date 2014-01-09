83ca9eabf97f56f953f016e45ed6962b3d2f2b30
Leonardo de Moura <leonardo@microsoft.com>
2014-01-09 11:50:07 -0800

    feat(emacs): include lean-mode Emacs files in the distribution
    Signed-off-by: Leonardo de Moura <leonardo@microsoft.com>

---
+++ lean-mode.el
@@ -0,0 +1,39 @@
+(require 'generic-x)
+(require 'lean-input)
+(require 'compile)
+
+(defvar lean-exe "lean"
+  "Path for the Lean executable")
+
+(defun lean-execute ()
+  "Execute Lean in the current buffer"
+  (interactive)
+  (compile (format "%s %s" lean-exe (buffer-file-name))))
+
+(defun lean-set-keys ()
+  (local-set-key "\C-c\C-x" 'lean-execute)
+  (local-set-key "\C-c\C-l" 'lean-execute))
+
+(define-generic-mode
+    'lean-mode     ;; name of the mode to create
+  '("--")          ;; comments start with
+  '("import" "definition" "variable" "variables" "print" "axiom" "theorem" "universe" "alias" "help" "set_option" "set_opaque" "environment" "options" "infix" "infixl" "infixr" "notation" "eval" "check" "exit" "coercion" "end" "using" "namespace" "builtin" "scope" "pop_scope") ;; some keywords
+  '(("\\<\\(Bool\\|Int\\|Nat\\|Real\\|Type\\|TypeU\\|ℕ\\|ℤ\\)\\>" . 'font-lock-type-face)
+    ("\\<\\(calc\\|have\\|in\\|let\\)\\>" . font-lock-keyword-face)
+    ("\"[^\"]*\"" . 'font-lock-string-face)
+    ("\\(->\\|/\\\\\\|==\\|\\\\/\\|[*+/:<=>¬λ→∀∃∧∨≠≤≥-]\\)" . 'font-lock-constant-face))
+  '("\\.lean$")                    ;; files for which to activate this mode
+  '((lambda()
+      (set-input-method "Lean")
+      (set (make-local-variable 'lisp-indent-function)
+           'common-lisp-indent-function)
+      (lean-set-keys)
+      ))
+  "A mode for Lean files"          ;; doc string for this mode
+  )
+
+(provide 'lean-mode)
+
+; (regexp-opt '("Int" "Bool" "Nat" "Type" "Real") t)
+; (regexp-opt '("let" "in" "have" "calc" "forall" "exists") t)
+; (regexp-opt '("=" "->" "≠" "∨" "∧" "¬" "/\\" "\\/" "+" "-" "*" "/" "<" ">" "≤" "≥" "==" "∀" "∃" "→" "λ" ":") t)
